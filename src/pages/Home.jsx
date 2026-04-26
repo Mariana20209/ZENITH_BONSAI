b35a2cc808f2234f1eaedd6a0227989f6d4b8bbc
Mariana20209 <mariasuarez20209@gmail.com>
2026-04-25 20:41:01 -0500

    Actualización del archivo home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <main className="hero-section">
+      <div className="hero-content">
+        <h1>El arte de la 
+            paciencia</h1>
+        <p>Descubre la serenidad a través del cuidado y diseño de árboles Bonsái. Un espacio para reconectar con la naturaleza.</p>
+        <a href="/reservations" className="btn">Reserva un taller</a>
+      </div>
+      <div className="hero-image"></div>
+    </main>
+  );
+};
+
+export default Home;
